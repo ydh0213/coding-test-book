2c28ce6f30fbe92942136828d46c5d691e836104
Yoondy <ydh0213@gmail.com>
2021-12-10 02:17:59 +0900

    PART 4 Chapter 02 기출문제 08 Python 코드 추가

---
+++ PART 4/Chapter 02/Problem 08/1.py
@@ -0,0 +1,6 @@
+def solution(s):
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i in range(10):
+        s = s.replace(numbers[i], str(i))
+
+    return int(s)
